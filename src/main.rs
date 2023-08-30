d9804f373e0678d074eb730d99b3a9347e51ce51
araxyso <69326871+Zinedinarnaut@users.noreply.github.com>
2023-08-30 21:03:13 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
- mod functions;
+mod functions;
 
 use actix_web::{web, App, HttpServer};
 use functions::{
     actix_web_module::{health_check, index},
     discord::{discord_notify, Handler},
-    load-balancer::LoadBalancer,
+    load_balancer::LoadBalancer, // Adjust the module path to match your actual module structure
 };
 use log::{error, info};
 use rand::Rng;
@@ -16,8 +16,10 @@ use serenity::{
     prelude::*,
 };
 use std::sync::{Arc, Mutex};
+use sqlx::PgPool; // Import SQLx for PostgreSQL
 
-fn main() -> std::io::Result<()> {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "info"); // Set log level to info
     env_logger::init();
 
@@ -26,12 +28,19 @@ fn main() -> std::io::Result<()> {
     // Replace this with your Discord channel ID
     let discord_channel = ChannelId(123456789012345678);
 
+    // Set up your Prisma client and SQLx connection pool
+    let prisma = prisma::PrismaClient::new();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPool::new(&database_url).await.expect("Failed to create pool");
+
     info!("Starting load balancer...");
 
     HttpServer::new(move || {
         App::new()
             .data(lb.clone())
             .data(discord_channel)
+            .data(prisma.clone()) // Pass the Prisma client to the Actix app
+            .data(pool.clone())   // Pass the SQLx pool to the Actix app
             .service(index)
     })
     .bind("127.0.0.1:8080")?
